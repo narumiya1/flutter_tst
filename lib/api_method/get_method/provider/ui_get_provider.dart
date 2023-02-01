72938f086a91ba5e70f5328a24518ac35e27913f
narumiya1 <narumiiyamei@gmail.com>
2023-02-01 15:12:06 +0700

    get method state & providr

---
+++ lib/api_method/get_method/provider/ui_get_provider.dart
@@ -0,0 +1,19 @@
+import 'package:diajar_flutter/api_method/get_method/provider/page_provider_get.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'http_get_provider.dart';
+
+class GetMethodProvider extends StatelessWidget {
+  const GetMethodProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ChangeNotifierProvider(
+        create: (context) => HttpGetProvider(),
+        child: HomeGetProvider(),
+      ),
+    );
+  }
+}
